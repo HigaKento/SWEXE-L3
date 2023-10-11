4e6c620d9eee91d5a5bf74003c67ec7be9d264ca
Kento Higa <2111140057r@ed.fuk.kindai.ac.jp>
2023-10-11 08:54:31 +0000

    テスト

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,3 +68,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+
+config.hosts << "7605690fd8984c3199c08409e12fcf13.vfs.cloud9.us-east-1.amazonaws.com"
+  
